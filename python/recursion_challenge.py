e63a3849b3b0c91d2de2ca7ec601e329e20bfead
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-09 20:12:56 -0500

    python palindrome

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -4,10 +4,18 @@ def factorial(x):
 	else:
 		return x * factorial(x - 1)
 
-print(factorial(7))
+#print(factorial(7)) # Equals 5040
 
 def palindrome(string):
-	pass
+	if len(string) <= 1:		# empty string and single letter are palindromes
+		return True
+	if string[0] != string[len(string) - 1]:  # first and last letters don't match
+		return False
+	return palindrome(string[1:-1])
+
+	
+
+print(palindrome('abcba'))	
 
 def bottles(num):
 	pass
