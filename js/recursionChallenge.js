dbc0809830ecac168defbb67e3415c60696226bf
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-09 21:48:40 -0500

    js bottles

--- js/recursionChallenge.js
+++ js/recursionChallenge.js
@@ -19,11 +19,22 @@ exports.palindrome = function(string) {
     return exports.palindrome(string.slice(1,-1));
 };
 
-console.log(exports.palindrome('able was i ere i saw elba'));
+//console.log(exports.palindrome('able was i ere i saw elba'));
 
-exports.bottles = function() {
+exports.bottles = function(num) {
+    if ( num === 1){
+        return(console.log("Take one down and pass it around, 1 bottle of beer on the wall. \n1 bottle of beer on the \
+wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the  \
+wall. \nNo more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy \
+some more, 99 bottles of beer on the wall."));
+    }
+    console.log(`${num} Bottles of beer on the wall, ${num} bottles of beer.\nTake one \
+down pass it around, ${num - 1} more on the wall.`);
 
+    exports.bottles(num - 1);
 };
+
+console.log(exports.bottles(99));
 
 exports.romanNum = function() {
 
