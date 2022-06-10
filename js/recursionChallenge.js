52e57d78cffd185085fad6894b3ab87eafb63fc5
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-09 21:36:56 -0500

    js palindrome

--- js/recursionChallenge.js
+++ js/recursionChallenge.js
@@ -1,10 +1,25 @@
-exports.factorial = function() {
-
+exports.factorial = function(num) {
+    if (num === 1){
+        return num;
+    } else {
+        return num * exports.factorial(num - 1);
+    }
+    
 };
 
-exports.palindrome = function() {
+// console.log(exports.factorial(7));
 
+exports.palindrome = function(string) {
+    if (string.length <= 1){
+        return(true)
+    }
+    if (string[0] !== string[string.length - 1]){  // first and last letters don't match
+		return(false)
+    }
+    return exports.palindrome(string.slice(1,-1));
 };
+
+console.log(exports.palindrome('able was i ere i saw elba'));
 
 exports.bottles = function() {
 
